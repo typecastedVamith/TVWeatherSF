4017e44ec0103a89de30fd826a38ae04b2288b1d
typecastedVamith <amith.v.nayak@gmail.com>
2020-07-07 13:04:08 +0530

    Added initial project setup

---
+++ TVWeatherSF/Feature/WeatherValidation.feature
@@ -0,0 +1,10 @@
+﻿Feature: WeatherValidation
+	This feature validates weather from multiple sources for a city
+
+@mytag
+Scenario: Compare weather forcast from multiple sources of a metro city
+	Given I read weatherForcast from ndtv weather website of an metro city
+	When I read weatherforcast of of op	enWeather API of metro city
+	Then I validate the weatherforcast from both the sources
+
+	
---
+++ TVWeatherSF/StepDefinition/WeatherValidationStep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace TVWeatherSF.StepDefinition
+{
+    class WeatherValidationStep
+    {
+        [Given(@"I read weatherForcast from ndtv weather website of an metro city")]
+        public void GivenIReadWeatherForcastFromNdtvWeatherWebsiteOfAnMetroCity()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I read weatherforcast of of op\tenWeather API of metro city")]
+        public void WhenIReadWeatherforcastOfOfOpEnWeatherAPIOfMetroCity()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I validate the weatherforcast from both the sources")]
+        public void ThenIValidateTheWeatherforcastFromBothTheSources()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
